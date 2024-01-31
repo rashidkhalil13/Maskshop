d583ae81e2b53be601facf170af7291a22b3142b
shrimplypibbles69 <121933560+shrimplypibbles69@users.noreply.github.com>
2024-02-01 00:09:42 +0100

    adding loading componenents, error messgaes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,6 +6,8 @@ import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
 import Rating from '../components/Ratings';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 // import data from '../data';
 
@@ -51,9 +53,9 @@ function HomeScreen() {
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
-          <div>Loading...</div>
+          <LoadingBox />
         ) : error ? (
-          <div>{error}</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row>
             {products.map((product) => (
